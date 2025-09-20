837d197d9f22f95a62f6bd0a3df7c3b6a9f5e6cd
Harshil Gupta <harshilgupta1808@gmail.com>
2025-09-20 21:36:49 +0530

    remember changed username

--- game-client/game.js
+++ game-client/game.js
@@ -55,6 +55,14 @@ class NakamaGame {
         }
         this.elements.username.value = username;
 
+    // Remember username if changed
+    this.elements.username.addEventListener('change', (e) => {
+        const newUsername = e.target.value.trim();
+        if (newUsername) {
+            localStorage.setItem('username', newUsername);
+        }
+    });
+
         this.elements.newGameBtn = document.getElementById('new-game-btn');
         this.elements.newGameBtn.addEventListener('click', () => {
             this.elements.newGameBtn.classList.add('hidden');
@@ -62,7 +70,7 @@ class NakamaGame {
             this.startMatchmaking();
         });
 
-        this.leaderboardPanel = document.getElementById('leaderboard-panel');
+        this.leaderboardPanel = document.getElementById('leaderboard-panel');ß
         this.leaderboardTable = document.getElementById('leaderboard-table');
 
         // Add event listeners
